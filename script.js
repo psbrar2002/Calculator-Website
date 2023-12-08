9a4c6fb9394a0e7f4662295e93e81a8fcb6bf874
Prabhdeep Brar <96799357+psbrar2002@users.noreply.github.com>
2023-12-07 21:46:49 -0800

    Update script.js
    Updated

--- script.js
+++ script.js
@@ -1,63 +1,67 @@
 let display = document.getElementById('display');
-    let currentInput = '';
+let currentInput = '';
 
-    function appendToDisplay(value) {
-      currentInput += value;
-      display.value = currentInput;
-    }
+function appendToDisplay(value) {
+  currentInput += value;
+  display.value = currentInput;
+}
 
-    function setOperator(op) {
-      currentInput += ' ' + op + ' ';
-      display.value = currentInput;
-    }
+function setOperator(op) {
+  currentInput += ' ' + op + ' ';
+  display.value = currentInput;
+}
 
-    function applyPercent() {
-      try {
-        const expression = currentInput.split(' ');
-        let result = parseFloat(expression[0]);
+function applyPercent() {
+  try {
+    const expression = currentInput.split(' ');
+    let result = parseFloat(expression[0]);
 
-        for (let i = 1; i < expression.length; i += 2) {
-          const operator = expression[i];
-          const operand = parseFloat(expression[i + 1]);
+    for (let i = 1; i < expression.length; i += 2) {
+      const operator = expression[i];
+      const operand = parseFloat(expression[i + 1]);
 
-          if (operator === '*' || operator === '/') {
-            // If the operator is '*' or '/', apply percentage correctly
-            const percentValue = operand / 100;
-            result = operator === '*' ? result * percentValue : result / percentValue;
-          } else {
-            // For other operators, perform regular calculations
-            switch (operator) {
-              case '+':
-                result += operand;
-                break;
-              case '-':
-                result -= operand;
-                break;
-              default:
-                display.value = 'Error';
-                return;
-            }
-          }
+      if (operator === '*' || operator === '/') {
+        // If the operator is '*' or '/', apply percentage correctly
+        const percentValue = operand / 100;
+        result = operator === '*' ? result * percentValue : result / percentValue;
+      } else {
+        // For other operators, perform regular calculations
+        switch (operator) {
+          case '+':
+            result += operand;
+            break;
+          case '-':
+            result -= operand;
+            break;
+          default:
+            display.value = 'Error';
+            return;
         }
-
-        display.value = result;
-        currentInput = result.toString();
-      } catch (error) {
-        display.value = 'Error';
       }
     }
 
-    function clearDisplay() {
-      currentInput = '';
-      display.value = '';
-    }
+    display.value = result;
+    currentInput = result.toString();
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
 
-    function calculate() {
-      try {
-        const result = eval(currentInput);
-        display.value = result;
-        currentInput = result.toString();
-      } catch (error) {
-        display.value = 'Error';
-      }
-    }
+function clearDisplay() {
+  currentInput = '';
+  display.value = '';
+}
+
+function calculate() {
+  try {
+    const result = eval(currentInput);
+    display.value = result;
+    currentInput = result.toString();
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+
+
+
+
